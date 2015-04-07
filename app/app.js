49712d74ce797da5366612bbe2406753bc7cc71c
Dorthu <dorthu2@gmail.com>
2015-04-06 22:22:01 -0400

    Added display of known issues in sidebar - now users can see what we already know is broken!

--- app/app.js
+++ app/app.js
@@ -10,9 +10,24 @@ angular.module('ecnonmizer', [
   'mobile-angular-ui',
   'mobile-angular-ui.components'
 ]).
-config(['$routeProvider', function($routeProvider, $cookies) {
+config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/index', {
       templateUrl: 'home/home.html'
     });
     $routeProvider.otherwise({redirectTo: '/index'});
+}])
+
+.controller('SidebarCtrl', ['$scope', '$http', function($scope, $http) {
+    ///pass
+    $scope.knownIssues = [];
+
+    $http.get("https://api.github.com/repos/Dorthu/FuelEconomizer/issues?labels=bug", '', '')
+        .success(function(data, status, headers, config) {
+            $scope.knownIssues = $scope.knownIssues.concat(data);
+        });
+
+    $http.get("https://api.github.com/repos/Dorthu/EconomizerWeb/issues?labels=bug", '', '')
+        .success(function(data, status, headers, config) {
+            $scope.knownIssues = $scope.knownIssues.concat(data);
+        });
 }]);
